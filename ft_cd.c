10ee0d1a3b3add8fcd247b38d1cbd4fcf71c7ce7
Livien LEVERGEOIS <lleverge@student.42.fr>
2016-04-06 15:26:29 +0200

    Added cd - gestion + normed

--- ft_cd.c
+++ ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: lleverge <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/21 13:58:26 by lleverge          #+#    #+#             */
-/*   Updated: 2016/03/21 16:17:13 by lleverge         ###   ########.fr       */
+/*   Updated: 2016/04/06 15:25:33 by lleverge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,48 @@
 
 static void		ft_cd_error(char *name)
 {
-	ft_putstr_fd("Error:", 2);
 	ft_putstr_fd(name, 2);
-	ft_putstr_fd("Not such directory", 2);
+	ft_putstr_fd(": No such file or directory.", 2);
 	ft_putchar('\n');
+}
+
+static void		ft_cd_prev(t_env *env)
+{
+	char	*tmp;
+
+	while (env)
+	{
+		if (ft_strcmp(env->name, "OLDPWD") == 0)
+		{
+			tmp = env->content;
+			change_varcontent(env, "OLDPWD", getwd(NULL));
+			chdir(tmp);
+			change_varcontent(env, "PWD", getwd(NULL));
+		}
+		env = env->next;
+	}
+}
+
+static void		ft_cd_home(t_env *env)
+{
+	while (env)
+	{
+		if (ft_strcmp(env->name, "HOME") == 0)
+		{
+			change_varcontent(env, "OLDPWD", getwd(NULL));
+			chdir(env->content);
+			change_varcontent(env, "PWD", getwd(NULL));
+		}
+		env = env->next;
+	}
 }
 
 void			ft_cd(char *moveto, t_env *env)
 {
 	if (!moveto || (ft_strcmp(moveto, "~") == 0))
-	{
-		while (env)
-		{
-			if (ft_strcmp(env->name, "HOME") == 0)
-			{
-				change_varcontent(env, "OLDPWD", getwd(NULL));
-				chdir(env->content);
-				change_varcontent(env, "PWD", getwd(NULL));
-			}
-			env = env->next;
-		}
-	}
+		ft_cd_home(env);
+	else if (ft_strcmp(moveto, "-") == 0)
+		ft_cd_prev(env);
 	else if (opendir(moveto) == NULL)
 	{
 		ft_cd_error(moveto);
